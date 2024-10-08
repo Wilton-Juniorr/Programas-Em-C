b6caf0b02d4782f0c27c1382511623991f02564a
Wilton Junior <wiltonjdrjunior@gmail.com>
2024-10-08 12:17:01 -0300

    Exer2
    //Faça um programa em C para calcular a média aritmética de três números inteiros
    adicionei também o setlocale para funcionar os acentos

---
+++ Exercicios/Exer2.c
@@ -0,0 +1,29 @@
+//Faça um programa em C para calcular a média aritmética de três números inteiros.
+#include <stdio.h>
+#include <locale.h> //Biblioteca para selecionar uma lingua
+int main(){
+    setlocale(LC_ALL, "Portuguese"); //isso fara com que os acento e ç 
+int n1,n2,n3,M;
+
+
+
+
+
+
+
+    printf("Digite o primeiro número inteiro:\n");
+    scanf("%d", &n1);
+
+    printf("Digite o Segundo número inteiro:\n");
+    scanf("%d", &n2);
+
+    printf("Digite o Terceiro número inteiro:\n");
+    scanf("%d", &n3);
+
+    M = (n1 + n2 + n3)/3;
+
+    printf("A média Aritmética é");
+    printf("Resultado: %d", M);
+
+    return 0;
+}
