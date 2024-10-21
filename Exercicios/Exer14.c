f5ec7f67e2dcf480e826002b995b584e28289072
Wilton Junior <wiltonjdrjunior@gmail.com>
2024-10-21 16:25:58 -0300

    Exer14

---
+++ Exercicios/Exer14.c
@@ -0,0 +1,59 @@
+/*Faça um switch case com os seguintes exercicios.
+1. Verificação de Par ou Ímpar
+Escreva um programa que receba um número inteiro do usuário e use a estrutura if-else para verificar se o número é par ou ímpar. O programa deve exibir o resultado correspondente.
+
+2. Calculadora Simples com switch
+Crie um programa que simule uma calculadora simples. O usuário deve inserir dois números e uma operação (adição, subtração, multiplicação ou divisão). Use o switch para realizar a operação correta e exibir o resultado.
+
+3. Verificar Maior de Três Números
+Escreva um programa que receba três números inteiros e use if-else para determinar e exibir qual deles é o maior.
+
+4. Imprimir Números de 1 a 100 (Com for)
+Crie um programa que imprima todos os números de 1 a 100 usando a estrutura for.
+
+5. Soma de Números Pares (Com while)
+Escreva um programa que calcule e exiba a soma de todos os números pares de 1 a 100. Use a estrutura while para realizar a soma.
+
+6. Número Fatorial (Com do-while)
+Escreva um programa que peça ao usuário um número inteiro positivo e calcule o fatorial desse número usando a estrutura do-while. O fatorial de um número n é a multiplicação de todos os números de 1 até 𝑛.
+
+7. Menu de Opções (Com switch)
+Crie um programa que exiba um menu com as seguintes opções:
+
+Verificar se um número é positivo ou negativo.
+Verificar se um número é par ou ímpar.
+Sair do programa. O programa deve usar switch para escolher a opção e realizar a ação correspondente.
+
+8. Cálculo de Média Aritmética
+Escreva um programa que receba quatro notas de um aluno e calcule a média aritmética. O programa deve usar if-else para verificar se a média é maior ou igual a 7.0 e exibir se o aluno está aprovado ou reprovado.
+
+9. Tabuada de um Número (Com for)
+Escreva um programa que peça ao usuário um número inteiro e exiba a tabuada desse número de 1 a 10, usando a estrutura for.
+
+10. Soma de Números até Zero (Com do-while)
+Crie um programa que receba números inteiros do usuário e vá somando esses números. O programa deve parar de pedir números e exibir a soma total quando o usuário inserir o número 0. Use a estrutura do-while para realizar essa tarefa.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void linha(){
+    printf("----------------------------------\n");
+}
+
+int main(){
+    int opcao;
+
+    linha();
+    printf("\tMENU DE OPCOES\n");
+    printf("\t1.Par ou Impar\n");
+    scanf("%d", &opcao);
+    linha();
+
+    switch(opcao){
+        case 1:{
+            
+        }
+    }
+
+    return 0;
+}
